2634b96eb70b24cb68b27d305d615c21348da980
polyqubit <polyumbrail@gmail.com>
2021-12-01 12:47:25 -0800

    edit starter

--- starter.java
+++ starter.java
@@ -1,29 +1,9 @@
-import pkg.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class starter implements InputControl, InputKeyControl {
+public class starter {
 
 	public static void main(String args[]) {
-		// please leave following line alone, necessary for keyboard/mouse input
-		KeyController kC = new KeyController(Canvas.getInstance(),new starter());
-		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
-		int one = 1;
-double five = 5.0;
-System.out.print("This is a potato");
-System.out.println("I love my potato");
-System.out.println("This potato is my life");
-System.out.print(one + five);
-System.out.println(five + " = five");
-	}
-
-	public void onMouseClick(double x, double y) {
-		// enter code here
-
-	}
-
-	public void keyPress(String s) {
-		// enter code here
-
+		
 	}
 }

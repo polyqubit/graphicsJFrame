2d14d8dd5f298284d15370db983df231bc825593
polyqubit <polyumbrial@gmail.com>
2021-12-11 10:02:13 -0800

    learning swing now nvm

--- starter.java
+++ starter.java
@@ -1,9 +1,15 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
+import java.util.*;
+import javax.swing.*;
+import start.*;
 public class starter {
 
-	public static void main(String args[]) {
-		
-	}
+	public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                SwingTest.createAndShowGUI();
+            }
+        });
+    }
 }
